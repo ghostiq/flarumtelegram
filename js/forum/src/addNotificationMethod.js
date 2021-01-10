ba86566169c6105c43d55bfeac45af0d591e9475
Czarodziej Nocy <6280789+ghostiq@users.noreply.github.com>
2021-01-10 20:21:03 +0100

    dexif

--- js/forum/src/addNotificationMethod.js
+++ js/forum/src/addNotificationMethod.js
@@ -2,35 +2,53 @@ import {extend} from 'flarum/extend';
 import app from 'flarum/app';
 import NotificationGrid from 'flarum/components/NotificationGrid';
 import SettingsPage from 'flarum/components/SettingsPage';
+import LogInButton from 'flarum/components/LogInButton';
 
 export default function () {
-    // Given there's currently no way to extend the list of methods and that the list needs to be complete at the end of init()
-    // We tap into notificationTypes() that is run between the creation of this.methods and the loop that reads them at the end of init()
-    extend(NotificationGrid.prototype, 'notificationTypes', function () {
+    extend(NotificationGrid.prototype, 'notificationMethods', function (items) {
         if (!app.forum.attribute('flagrow-telegram.enableNotifications')) {
             return;
         }
 
-        const user = app.session.user;
-
+        let user = app.session.user;
         if (!user || !user.canReceiveTelegramNotifications()) {
             return;
         }
 
-        this.methods.push({
+        // Add telegram notifications method column
+        items.add('telegram', {
             name: 'telegram',
-            icon: 'telegram',
+            icon: 'fab fa-telegram-plane',
             label: app.translator.trans('flagrow-telegram.forum.settings.notify_by_telegram_heading'),
         });
     });
-
+    extend(SettingsPage.prototype, 'accountItems', function (items) {
+        if (!app.forum.attribute('flagrow-telegram.enableNotifications')) {
+            return;
+        }
+        let user = app.session.user;
+        if (user && !user.canReceiveTelegramNotifications()) {
+            // add button to link current account with telegram
+            items.add('flagrow-telegram',
+                <div>
+                    <LogInButton
+                        className="Button LogInButton--telegram"
+                        style="display:inline-block; width:auto"
+                        icon="fab fa-telegram-plane"
+                        path="/auth/telegram">
+                        {app.translator.trans('flagrow-telegram.forum.link_telegram_button')}
+                    </LogInButton>
+                </div>
+            );
+        }
+    });
     extend(SettingsPage.prototype, 'notificationsItems', function (items) {
+
         if (!app.forum.attribute('flagrow-telegram.enableNotifications')) {
             return;
         }
 
-        const user = app.session.user;
-
+        let user = app.session.user;
         if (!user || !user.flagrowTelegramError()) {
             return;
         }
@@ -47,3 +65,4 @@ export default function () {
         });
     });
 }
+
